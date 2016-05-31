9960696ea41a5d3713aa918b6f72ad667d84e817
wxmimperio <wxmimperio@outlook.com>
2016-05-31 16:04:11 +0800

    "修改编码格式"

--- java-demo/com/utils/GetPath.java
+++ java-demo/com/utils/GetPath.java
@@ -4,16 +4,16 @@ package com.utils;
  * Created by wxmimperio on 2016/5/31.
  */
 public class GetPath {
-    public String getCurrentPath(){
+    public String getCurrentPath() {
         //取得根目录路径
-        String rootPath=getClass().getResource("/").getFile().toString();
+        String rootPath = getClass().getResource("/").getFile().toString();
 
         //当前目录路径
-        String currentPath1=getClass().getResource(".").getFile().toString();
-        String currentPath2=getClass().getResource("").getFile().toString();
+        String currentPath1 = getClass().getResource(".").getFile().toString();
+        String currentPath2 = getClass().getResource("").getFile().toString();
         //当前目录的上级目录路径
-        String parentPath=getClass().getResource("../").getFile().toString();
+        String parentPath = getClass().getResource("../").getFile().toString();
 
-        return parentPath;
+        return rootPath;
     }
 }
