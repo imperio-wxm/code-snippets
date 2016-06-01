a690b5508494ec984366bf67f1e452495370736f
wxmimperio <wxmimperio@outlook.com>
2016-06-01 09:32:36 +0800

    "更新GetPath"

--- java-demo/com/utils/GetPath.java
+++ java-demo/com/utils/GetPath.java
@@ -10,7 +10,7 @@ public class GetPath {
 
         //当前目录路径
         String currentPath1 = getClass().getResource(".").getFile().toString();
-        String currentPath2 = getClass().getResource("").getFile().toString();
+
         //当前目录的上级目录路径
         String parentPath = getClass().getResource("../").getFile().toString();
 
