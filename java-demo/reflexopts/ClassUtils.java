b23b9a39ec279be727cbf4afb0166af85f801f39
wxmimperio <wxmimperio@outlook.com>
2016-06-03 16:57:07 +0800

    "添加构造函数类类型的工具类"

--- java-demo/reflexopts/ClassUtils.java
+++ java-demo/reflexopts/ClassUtils.java
@@ -1,10 +1,15 @@
 package reflexopts;
 
+import org.omg.CORBA.*;
+
+import java.lang.Object;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 
 /**
  * Created by wxmimperio on 2016/6/3.
- *
+ * <p/>
  * 获取类基本信息的封装
  */
 public class ClassUtils {
@@ -13,7 +18,9 @@ public class ClassUtils {
      *
      * @param object
      */
-    public static void printClassInfo(Object object) {
+
+    //获取类方法的信息
+    public static void printMethodClassInfo(Object object) {
         //要获取类的基本信息，首先要获取类的类类型
         //传递的是哪个子类的对象，获取的就是该子类的类类型
         Class objClass = object.getClass();
@@ -45,6 +52,67 @@ public class ClassUtils {
                 System.out.println("参数列表的类类型：" + paramsClass + " " + "参数列表的类型名称：" + paramsClass.getName());
             }
 
+            System.out.println("--------------------------------------------");
+        }
+    }
+
+    //获取类成员变量的信息
+    public static void printClassParamsInfo(Object object) {
+        //要获取类的基本信息，首先要获取类的类类型
+        //传递的是哪个子类的对象，获取的就是该子类的类类型
+        Class objClass = object.getClass();
+        /**
+         * 成员变量也是对象
+         * java.lang.reflect.Field
+         * Field封装了关于成员变量的操作
+         * getFields获取的是所有public的成员变量
+         * getDeclaredFields获取所有自己声明的成员变量
+         */
+        //Field[] fs = objClass.getFields();
+        Field[] fs = objClass.getDeclaredFields();
+
+        for (Field field : fs) {
+            //获取成员变量类型的类类型和类型的名称
+            Class fieldType = field.getType();
+            System.out.println("成员变量类型的类类型：" + fieldType + " " + "成员变量类型的名称：" + fieldType.getName());
+
+            //得到成员变量的名字
+            System.out.println("成员变量的名字：" + field.getName());
+
+            System.out.println("--------------------------------------------");
+        }
+    }
+
+    /**
+     * 打印对象构造函数的信息
+     *
+     * @param object
+     */
+    public static void printClassConInfo(Object object) {
+        //要获取类的基本信息，首先要获取类的类类型
+        //传递的是哪个子类的对象，获取的就是该子类的类类型
+        Class objClass = object.getClass();
+
+        /**
+         * 构造函数也是对象
+         * java.lang.Constructor封装了构造函数的信息
+         * getConstructors获得所public的构造方法
+         * getDeclaredConstructors得到所有构造方法
+         */
+        //Constructor[] cs = objClass.getConstructors();
+        Constructor[] cs = objClass.getDeclaredConstructors();
+
+        for (Constructor constructor : cs) {
+            //获取构造函数的名称
+            System.out.println("构造函数的名称：" + constructor.getName());
+
+            //获取构造函数的参数列表的类类型和参数类型的名称
+            Class[] paramsTypes = constructor.getParameterTypes();
+
+            for (Class param : paramsTypes) {
+                //获取参数的名称
+                System.out.println("构造函数参数的名称：" + param.getName());
+            }
             System.out.println("--------------------------------------------");
         }
     }
