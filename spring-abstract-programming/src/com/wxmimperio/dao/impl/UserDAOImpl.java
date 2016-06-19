933a2d943b4c09a7fee7e7020861730c3630871c
wxmimperio <wxmimperio@outlook.com>
2016-06-19 14:56:54 +0800

    “添加spring面向接口编程实例”

---
+++ spring-abstract-programming/src/com/wxmimperio/dao/impl/UserDAOImpl.java
@@ -0,0 +1,15 @@
+package com.wxmimperio.dao.impl;
+
+import com.wxmimperio.dao.UserDAO;
+import com.wxmimperio.model.User;
+
+/**
+ * Created by wxmimperio on 2016/6/19.
+ * DAO层的实现
+ */
+public class UserDAOImpl implements UserDAO{
+    @Override
+    public void save(User user) {
+        System.out.println("a usr save");
+    }
+}
