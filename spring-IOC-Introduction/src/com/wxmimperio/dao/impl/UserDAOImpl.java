7089023b83782403be97ede1aadff40bb5b1d13f
wxmimperio <wxmimperio@outlook.com>
2016-06-19 20:08:33 +0800

    “注入简单属性”

--- spring-IOC-Introduction/src/com/wxmimperio/dao/impl/UserDAOImpl.java
+++ spring-IOC-Introduction/src/com/wxmimperio/dao/impl/UserDAOImpl.java
@@ -8,6 +8,26 @@ import com.wxmimperio.model.User;
  * DAO层的实现
  */
 public class UserDAOImpl implements UserDAO{
+
+    private int daoId;
+    private String daoStatus;
+
+    public int getDaoId() {
+        return daoId;
+    }
+
+    public void setDaoId(int daoId) {
+        this.daoId = daoId;
+    }
+
+    public String getDaoStatus() {
+        return daoStatus;
+    }
+
+    public void setDaoStatus(String daoStatus) {
+        this.daoStatus = daoStatus;
+    }
+
     @Override
     public void save(User user) {
         System.out.println("a usr save");
