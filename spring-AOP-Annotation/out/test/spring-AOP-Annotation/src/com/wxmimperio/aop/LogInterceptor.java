fb4bf8fa1a1f16db335f9fe7e454323f11a4da14
wxmimperio <wxmimperio@outlook.com>
2016-06-26 12:01:32 +0800

    " 织入点after添加测试"

--- spring-AOP-Annotation/out/test/spring-AOP-Annotation/src/com/wxmimperio/aop/LogInterceptor.java
+++ spring-AOP-Annotation/out/test/spring-AOP-Annotation/src/com/wxmimperio/aop/LogInterceptor.java
@@ -1,5 +1,6 @@
 package com.wxmimperio.aop;
 
+import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
@@ -8,7 +9,9 @@ import org.springframework.stereotype.Component;
  * Created by wxmimperio on 2016/6/25.
  */
 
+//声明这是切面逻辑
 @Aspect
+//对代理进行初始化
 @Component
 public class LogInterceptor {
     //在方法执行之前
@@ -16,5 +19,10 @@ public class LogInterceptor {
     @Before("execution(public void com.wxmimperio.dao.impl.UserDAOImpl.save(com.wxmimperio.model.User))")
     public void before() {
         System.out.println("method before");
+    }
+
+    @After("execution(public void com.wxmimperio.dao.impl.UserDAOImpl.save(com.wxmimperio.model.User))")
+    public void after() {
+        System.out.println("method after");
     }
 }
--- spring-AOP-Annotation/src/com/wxmimperio/aop/LogInterceptor.java
+++ spring-AOP-Annotation/src/com/wxmimperio/aop/LogInterceptor.java
@@ -1,5 +1,6 @@
 package com.wxmimperio.aop;
 
+import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
@@ -18,5 +19,10 @@ public class LogInterceptor {
     @Before("execution(public void com.wxmimperio.dao.impl.UserDAOImpl.save(com.wxmimperio.model.User))")
     public void before() {
         System.out.println("method before");
+    }
+
+    @After("execution(public void com.wxmimperio.dao.impl.UserDAOImpl.save(com.wxmimperio.model.User))")
+    public void after() {
+        System.out.println("method after");
     }
 }
