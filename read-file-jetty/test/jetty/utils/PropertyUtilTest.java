977a054e89b128a846874f0d3abe8ac37dde4953
wxmimperio <wxmimperio@outlook.com>
2016-07-14 16:50:07 +0800

    "添加文件过滤获取的test"

--- read-file-jetty/test/jetty/utils/PropertyUtilTest.java
+++ read-file-jetty/test/jetty/utils/PropertyUtilTest.java
@@ -1,7 +1,14 @@
 package jetty.utils;
 
 import com.wxmimperio.jetty.utils.PropertyUtil;
+import org.apache.commons.io.filefilter.RegexFileFilter;
 import org.junit.Test;
+
+import java.io.File;
+import java.io.FileFilter;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 /**
  * Created by weiximing.imperio on 2016/7/14.
@@ -15,5 +22,23 @@ public class PropertyUtilTest {
 
         System.out.println("port=" + port);
         System.out.println("path=" + path);
+    }
+
+    @Test
+    public void filterFilesTest() {
+        File dir = new File("E:/file_test/");
+        FileFilter fileFilter = new RegexFileFilter("^12399243.*.txt");
+        File[] files = dir.listFiles(fileFilter);
+        List list = new ArrayList();
+
+        for (int i = 0; i < files.length; i++) {
+            list.add(files[i].getName().split("\\.")[1]);
+        }
+        Collections.sort(list);
+        for(int i = 0; i < list.size(); i++) {
+            System.out.println(list.get(i));
+        }
+        System.out.println();
+        System.out.println(list.get(list.size()-1));
     }
 }
